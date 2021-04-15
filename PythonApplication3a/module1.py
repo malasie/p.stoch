78387dc50b2b9e7981ef274eba55040821343f56
Maria Malasiewicz <malasie@users.noreply.github.com>
2021-04-15 13:40:44 +0200

    wersja1

--- PythonApplication3a/module1.py
+++ PythonApplication3a/module1.py
@@ -10,15 +10,21 @@ def krok(p):
     else:
         return -1
 
-def spacer(P0, N, p):
+def spacer2(P0, N, p, a,b):
     poz=np.zeros([N+1,1])
     poz[0]=P0
     for i in range(1,N+1):
         poz[i]=poz[i-1]+krok(p)
+        if poz[i]==a:
+            return poz[0:i]
+        if poz[i]==b:
+            return poz[0:i+1]
     return poz
 
 fig=plt.figure(figsize=(8,4), dpi=200)
-plt.plot(spacer(0,100,0.5), 'b.')
-plt.plot(spacer(0,100,0.5), 'r.')
-plt.plot(spacer(0,100,0.5), 'g.')
+plt.plot(spacer2(0,1000,0.5,-10,10), 'b.')
+plt.plot(spacer2(0,1000,0.5,-10,10), 'r.')
+plt.plot(spacer2(0,1000,0.5,-10,10), 'g.')
+plt.plot(spacer2(0,1000,0.5,-10,10), 'c.')
+plt.plot(spacer2(0,1000,0.5,-10,10), 'm.')
 plt.show()
