855e35309b15c9c4a801dee818df3277f25550a1
Maria Malasiewicz <malasie@users.noreply.github.com>
2021-04-19 20:38:35 +0200

    ciąg dalszy

--- PythonApplication3a/praca domowa.py
+++ PythonApplication3a/praca domowa.py
@@ -10,6 +10,7 @@ def f(x):
     y=(5*x**2-50*x+105)/(x**2)
     return y
 
+#podpunkt a
 def calka(x):
     return 5*x-50*math.log(x)-105/x
 print(abs(calka(6)-calka(3)))
@@ -23,12 +24,12 @@ x2=6
 y1=f(210/50)
 y2=0
 
-print()
 
-N=5000
+#podpunkt b
+N=1000
 pole=[]
 
-for j in range(0,100):
+for j in range(0,N):
     X=np.random.uniform(x1,x2,N)
     Y=np.random.uniform(y1,y2,N)
     p=0
@@ -40,3 +41,12 @@ for j in range(0,100):
     pole.append((p/(p+z))*(x2-x1)*(y2-y1))
 
 print(pole)
+
+#podpunkt c
+plt.hist(pole,bins=20)
+plt.show()
+normalny=stats.norm(np.mean(pole),np.std(pole))
+print(stats.kstest(pole,normalny.cdf)) # pvalue>0.1 ->rozkład normalny
+
+# podpunkt d
+print(stats.ttest_1samp(pole,abs(calka(6)-calka(3))))
